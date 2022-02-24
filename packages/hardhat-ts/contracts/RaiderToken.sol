a22372d137bdd26f5bbba7f9c7055e304115fe93
KaneRodriguez <kanerodriguezpro@gmail.com>
2022-02-24 16:27:48 -0600

    Add reference to etherscan

--- packages/hardhat-ts/contracts/RaiderToken.sol
+++ packages/hardhat-ts/contracts/RaiderToken.sol
@@ -1,5 +1,7 @@
 //SPDX-License-Identifier: MIT
 
+// This code was taken from: https://polygonscan.com/address/0xcd7361ac3307d1c5a46b63086a90742ff44c63b3#code
+
 pragma solidity ^0.8.0;
 
 import './RaiderToken_ERC20.sol';
