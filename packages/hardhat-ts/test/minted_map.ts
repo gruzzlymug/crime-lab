7a49a93aca63dcc0d0e1d6ac2cc5c3f87553b380
gruzzlymug <kevin@gruzzlymug.com>
2022-02-23 18:13:51 -0600

    Add blitmap test

--- packages/hardhat-ts/test/minted_map.ts
+++ packages/hardhat-ts/test/minted_map.ts
@@ -52,8 +52,22 @@ describe("Loot", function () {
       const Blitmap = await ethers.getContractFactory("Blitmap");
       const blitmap = await Blitmap.deploy();
 
-      // TBD
+      let ba = [];
+      let i = 2;
+      // just drop some bytes from mintOriginal data here and go
+      // https://etherscan.io/txs?a=0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63&p=73
+      const bytesString = "0x0000009e2836e53b4463c74d0000000000000000000000000000000000000199998000000000199999980000000019999998000000001999999800000000159999580000000005599560000000000155558000000000001558000000000000038000000000000003f8000000000000003800000000000000380000000000000038e00000000000003fe00000000000003800000000000000f800000000000038e00000000000003fe000000000000000f800000000000000380000000000000038000000000000003800000000000000380000000000000038000000000000003800000000000000380000000000000038000000000000003800000000000000000000000000000000000000";
+      // convert to bytes array
+      while (i < bytesString.length - 1) {
+        const byte = bytesString.substring(i, i + 2);
+        const value = parseInt("0x" + byte, 16);
+        ba.push(value);
+        i += 2;
+      }
 
+      await blitmap.mintOriginal(ba, "on chain");
+      const svg = await blitmap.tokenSvgDataOf(0);
+      console.log(svg);
       expect(true).to.equal(true);
     });
   });
