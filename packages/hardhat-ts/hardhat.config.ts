ed2600bedb187d1802493b320277f0d1f3638ee8
KaneRodriguez <kanerodriguezpro@gmail.com>
2022-02-25 15:01:09 -0600

    update hardhat config

--- packages/hardhat-ts/hardhat.config.ts
+++ packages/hardhat-ts/hardhat.config.ts
@@ -144,6 +144,15 @@ const config: HardhatUserConfig = {
         },
       },
       {
+        version: '0.6.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: '0.5.16',
         settings: {
           optimizer: {
